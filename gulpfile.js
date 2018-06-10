7cf5dd93b97ac4f07b60ad903d22b6bf49fa1346
Luis Egan <luisdegan10@hotmail.com>
2018-06-10 18:34:57 -0400

    update to gulp 4.x

--- gulpfile.js
+++ gulpfile.js
@@ -6,37 +6,39 @@ const autoprefixer = require("gulp-autoprefixer");
 const minify = require("gulp-minify");
 const gulpSrc = "./src/scss/**/*.scss";
 
-gulp.task("sass", function() {
-   return gulp
-      .src(gulpSrc)
-      .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
-      .pipe(
-         autoprefixer({
-            browsers: ["last 2 versions"],
-            cascade: false
-         })
-      )
-      .pipe(gulp.dest("./src"));
+gulp.task("sass", function () {
+      return gulp
+            .src(gulpSrc)
+            .pipe(sass({
+                  outputStyle: "compressed"
+            }).on("error", sass.logError))
+            .pipe(
+                  autoprefixer({
+                        browsers: ["last 2 versions"],
+                        cascade: false
+                  })
+            )
+            .pipe(gulp.dest("./src"));
 });
 
-gulp.task("sass:watch", function() {
-   gulp.watch(gulpSrc, ["sass"]);
+gulp.task("sass:watch", function () {
+      gulp.watch(gulpSrc, gulp.series('sass'));
 });
 
-gulp.task("tjsminify", function() {
-   gulp
-      .src("./ThreeJsR92/*.js")
-      .pipe(
-         minify({
-            ext: {
-               min: ".min.js"
-            },
-            noSource: true
-         })
-      )
-      .pipe(gulp.dest("./public/THREEJS"));
+gulp.task("tjsminify", function () {
+      gulp
+            .src("./ThreeJsR92/*.js")
+            .pipe(
+                  minify({
+                        ext: {
+                              min: ".min.js"
+                        },
+                        noSource: true
+                  })
+            )
+            .pipe(gulp.dest("./public/THREEJS"));
 });
 
-gulp.task("tjsminify:watch", function() {
-   gulp.watch("./ThreeJsR92/*.js", ["tjsminify"]);
+gulp.task("tjsminify:watch", function () {
+      gulp.watch("./ThreeJsR92/*.js", gulp.series('tjsminify'));
 });
