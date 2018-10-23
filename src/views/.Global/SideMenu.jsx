2977c20d7486d966f36494fdb158662c131a9c05
Luis Egan <luisdegan10@hotmail.com>
2018-10-23 02:21:11 -0300

    add docus link to sidemenu

--- src/views/.Global/SideMenu.jsx
+++ src/views/.Global/SideMenu.jsx
@@ -1,27 +1,41 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import routeCodes from "../../config/routeCodes";
+import ReactSVG from "react-svg";
 
 import SVG from "../../components/SVG";
+import SVG_docs from "../../assets/svg/documentation.svg";
+
+function openInNewTab(url) {
+   const sideMenu = document.getElementById("sideMenu");
+   const a = document.createElement("a");
+   sideMenu.appendChild(a);
+   a.target = "_blank";
+   a.href = url;
+   a.click();
+   sideMenu.removeChild(a);
+}
 
 const sections = [
    {
       icon: SVG.navMyApps,
       title: "My apps",
-      pathname: "/myapps",
-      redirect: "MYAPPS"
+      pathname: routeCodes.MYAPPS
    },
    {
       icon: SVG.navMyProfile,
       title: "My profile",
-      pathname: "/profile",
-      redirect: "PROFILE"
+      pathname: routeCodes.PROFILE
+   },
+   {
+      icon: <ReactSVG src={SVG_docs} />,
+      title: "Documentation",
+      pathname: "https://docs.admix.in/"
    },
    {
       icon: SVG.navDownload,
       title: "Download",
-      pathname: "/download",
-      redirect: "DOWNLOAD"
+      pathname: routeCodes.DOWNLOAD
    }
 ];
 
@@ -35,8 +49,11 @@ class SideMenu extends Component {
       this.redirectTo = this.redirectTo.bind(this);
    }
 
-   redirectTo(redirect) {
-      this.props.history.push(routeCodes[redirect]);
+   redirectTo(pathname) {
+      if (pathname.indexOf("http") >= 0) {
+         openInNewTab(pathname);
+      }
+      this.props.history.push(pathname);
    }
 
    renderSections() {
@@ -52,7 +69,7 @@ class SideMenu extends Component {
             <div
                className={isSelected}
                key={section.title}
-               onClick={this.redirectTo.bind(null, section.redirect)}
+               onClick={this.redirectTo.bind(null, section.pathname)}
             >
                <div>{section.icon}</div>
                <div>{section.title}</div>
