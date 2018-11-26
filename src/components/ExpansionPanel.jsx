c11193d08a21e28a99e12ac07ea9f13335de9a26
Luis Egan <luisdegan10@hotmail.com>
2018-11-26 00:37:10 -0300

    create ExpansionPanel

---
+++ src/components/ExpansionPanel.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import ExpansionPanel from "@material-ui/core/ExpansionPanel";
+import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
+import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
+import FontAwesomeIcon from "@fortawesome/react-fontawesome";
+
+export default class ExpPanel extends React.Component {
+  render() {
+    const { headerIcon, headerTitle, contentId, Content } = this.props;
+    return (
+      <ExpansionPanel
+        defaultExpanded={true}
+        classes={{ root: "mui-expansionPanel-root" }}
+      >
+        <ExpansionPanelSummary expandIcon={<FontAwesomeIcon icon="angle-up" />}>
+          <div className="cc">
+            {headerIcon}
+            <span>{headerTitle}</span>
+          </div>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <div className="expansionPanelDetails-container" id={contentId}>
+            {Content}
+          </div>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+    );
+  }
+}
