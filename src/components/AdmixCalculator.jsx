b813ab111cb58380d46a177d88f8915a180e5ca3
Luis Egan <luisdegan10@hotmail.com>
2018-12-04 12:14:02 -0300

    use Field

--- src/components/AdmixCalculator.jsx
+++ src/components/AdmixCalculator.jsx
@@ -12,13 +12,6 @@ class AdmixCalculator extends React.Component {
       this.admixCut = 0;
       this.expected = 0;
 
-      this.state = {
-         cpm: 20,
-         mau: 0,
-         session: 0,
-         avg: 0
-      };
-
       this.changeInput = this.changeInput.bind(this);
    }
 
@@ -34,53 +27,59 @@ class AdmixCalculator extends React.Component {
       });
    }
 
-   render() {
-      const { cpm, mau, session, avg } = this.state;
+   renderField(field) {
+      const {
+         input,
+         meta: { error }
+      } = field;
 
-      this.impressions = mau * session * avg;
-      this.revenue = (this.impressions * cpm) / 1000;
-      this.admixCut = this.revenue / 5;
-      this.expected = this.revenue - this.admixCut;
+      return (
+         <div className="redux-form-inputs-container">
+            <Input
+               {...input}
+               id={input.name}
+            />
+         </div>
+      );
+   }
+
+   render() {
+      // this.impressions = mau * session * avg;
+      // this.revenue = (this.impressions * cpm) / 1000;
+      // this.admixCut = this.revenue / 5;
+      // this.expected = this.revenue - this.admixCut;
 
       return (
          <div className="admix-calculator">
             <div>Expected CPM ($)</div>
             <div>
-               <Input
-                  type="number"
+               <Field
+                  component={this.renderField}
                   name="cpm"
-                  onChange={this.changeInput}
-                  value={cpm}
                />
             </div>
 
             <div>MAU</div>
             <div>
-               <Input
-                  type="number"
+            <Field
+                  component={this.renderField}
                   name="mau"
-                  onChange={this.changeInput}
-                  value={mau}
                />
             </div>
 
             <div>Session per user / month</div>
             <div>
-               <Input
-                  type="number"
+            <Field
+                  component={this.renderField}
                   name="session"
-                  onChange={this.changeInput}
-                  value={session}
                />
             </div>
 
             <div>Avg ads per session</div>
             <div>
-               <Input
-                  type="number"
+            <Field
+                  component={this.renderField}
                   name="avg"
-                  onChange={this.changeInput}
-                  value={avg}
                />
             </div>
 
@@ -105,6 +104,10 @@ const mapStateToProps = (state, props) => {
 
    return {
       initialValues: {
+         cpm: 20,
+         mau: 0,
+         session: 0,
+         avg: 0,
          ...initialValues
       }
    };
