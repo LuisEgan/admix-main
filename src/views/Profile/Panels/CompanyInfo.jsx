10e3a70571e2541f24db2ccdf63debffbf325ae8
Luis Egan <luisdegan10@hotmail.com>
2018-12-11 00:13:42 -0300

    add onlyNums normalizer

--- src/views/Profile/Panels/CompanyInfo.jsx
+++ src/views/Profile/Panels/CompanyInfo.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Field } from "redux-form";
+import { onlyNums } from "../../../utils/normalizers"
 
 export default ({ renderField }) => {
   return (
     <React.Fragment>
       <div id="profile-company">
         <Field name="companyName" component={renderField} />
-        <Field name="registeredNumber" component={renderField} />
+        <Field name="registeredNumber" component={renderField} normalize={onlyNums}/>
         <Field name="VAT" component={renderField} />
         <Field name="address1" component={renderField} />
         <Field name="address2" component={renderField} />
