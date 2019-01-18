ccd1f87fb8ce11033b0a9c6ef0482224e241c0c8
LDEV <luisdegan10@hotmail.com>
2019-01-17 21:41:02 -0300

    prettier

--- src/config/store.js
+++ src/config/store.js
@@ -1,11 +1,5 @@
-import {
-    createStore,
-    applyMiddleware,
-    combineReducers
-} from "redux";
-import {
-    persistStore
-} from "redux-persist";
+import { createStore, applyMiddleware, combineReducers } from "redux";
+import { persistStore } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import immutableTransform from "redux-persist-transform-immutable";
 import thunk from "redux-thunk";
@@ -17,20 +11,20 @@ import persistReducer from "redux-persist/lib/persistReducer";
 const isProduction = process.env.NODE_ENV === "production";
 
 const logger = createLogger({
-    collapsed: true
-})
+  collapsed: true,
+});
 
-const middleware = isProduction ?
-    applyMiddleware(thunk) :
-    applyMiddleware(thunk, logger);
+const middleware = isProduction
+  ? applyMiddleware(thunk)
+  : applyMiddleware(thunk, logger);
 
 // Add redux-persist's configuration
 const config = {
-    transforms: [immutableTransform()],
-    key: "root",
-    storage,
-    whitelist: ["app"]
-    //    blacklist: ["app"]
+  transforms: [immutableTransform()],
+  key: "root",
+  storage,
+  whitelist: ["app"],
+  //    blacklist: ["app"]
 };
 
 const reducer = combineReducers(rootReducer);
@@ -40,6 +34,6 @@ const store = createStore(persistedReducer, middleware);
 let persistor = persistStore(store);
 
 export default () => ({
-    persistor,
-    store
+  persistor,
+  store,
 });
