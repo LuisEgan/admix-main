859d46b6f8bc1cae22450dac45e7fbbd43b0b3d3
LDEV <luisdegan10@hotmail.com>
2019-02-14 12:20:43 -0300

    dns

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,9 @@
 import qs from "qs";
 
 const onTestServer = window.location.host.includes("3001");
-const isProd = process.env.NODE_ENV !== "development" && !onTestServer;
+// const onTestServer = true;
+// const isProd = process.env.NODE_ENV !== "development" && !onTestServer;
+const isProd = true;
 
 const dns = isProd ? "https://api.admix.in" : !onTestServer ? "http://test.api.admix.in" : "http://localhost:3000";
 !isProd && console.warn("dns: ", dns);
