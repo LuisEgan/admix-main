7db6d08a94b8c5e817feffb2a46310445f4793b8
LDEV <luisdegan10@hotmail.com>
2019-02-19 23:29:13 -0300

    redirect to login

--- src/views/EmailVerification/emailSuccess.jsx
+++ src/views/EmailVerification/emailSuccess.jsx
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
+import { Redirect } from "react-router-dom"
+import routeCodes from "../../config/routeCodes";
 
-import OK from "../../assets/img/Thumbs_Up_Hand_Sign_Emoji.png";
+// import OK from "../../assets/img/Thumbs_Up_Hand_Sign_Emoji.png";
 
 export default class Congratulations extends Component {
   static propTypes = {};
 
   render() {
     return (
-      <div className="step-container" id="congratulations">
+      <React.Fragment>
         <noscript>
           <img
             src="https://www.facebook.com/tr?id=2233658986869535&ev=PageView"
@@ -17,22 +19,32 @@ export default class Congratulations extends Component {
             alt="fb"
           />
         </noscript>
-        <div className="container simple-container">
-          <h3 className="st">Verified!</h3>
-          <div>
-            <div className="emoji-container">
-              <img src={OK} alt="B-)" />
-            </div>
-            <h2 className="st">Success! You can log in now!</h2>
-            <h3 className="mb">
-              Right this way <br />
-            </h3>
-            <a href="/login" className="btn btn-dark">
-              Log in
-            </a>
-          </div>
-        </div>
-      </div>
+
+        <Redirect
+          to={{
+            pathname: routeCodes.LOGIN,
+            state: { from: routeCodes.EMAIL_SUCCESS },
+          }}
+        />
+      </React.Fragment>
     );
   }
 }
+
+// <div className="step-container" id="congratulations">
+//   <div className="container simple-container">
+//     <h3 className="st">Verified!</h3>
+//     <div>
+//       <div className="emoji-container">
+//         <img src={OK} alt="B-)" />
+//       </div>
+//       <h2 className="st">Success! You can log in now!</h2>
+//       <h3 className="mb">
+//         Right this way <br />
+//       </h3>
+//       <a href="/login" className="btn btn-dark">
+//         Log in
+//       </a>
+//     </div>
+//   </div>
+// </div>
