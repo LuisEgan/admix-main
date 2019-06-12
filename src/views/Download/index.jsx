b456bfb84253935a991942e5b32cda05ce2ed5c0
Andrii Medvediev <andrii.medvediev@dish.com>
2019-06-12 13:13:34 +0300

    Update dependencies and Unity plugin version

--- src/views/Download/index.jsx
+++ src/views/Download/index.jsx
@@ -50,7 +50,7 @@ class Download extends Component {
               <div>
                 <a
                   className="btn-dark mb"
-                  href="https://admix.in/wp-content/uploads/2019/04/Admix.Unity_Rev.1.7.3_RC3.unitypackage"
+                  href="http://admix.in/wp-content/uploads/2019/06/Admix.Unity_Rev.1.7.3.unitypackage"
                   target="_blank"
                   rel="noopener noreferrer"
                   onClick={this.handleDownload.bind(null, "unity")}
@@ -98,7 +98,7 @@ class Download extends Component {
           <div className="mb download-bottom-section">
             <div>
               <span className="subtitle">VERSION</span>
-              <span>Admix.Unity_Rev.1.7.3_RC3.unitypackage</span>
+              <span>Admix.Unity_Rev.1.7.3.unitypackage</span>
               <span className="subtitle">SIZE</span>
               <span>4MB</span>
               <span className="subtitle">REQUIREMENTS</span>
